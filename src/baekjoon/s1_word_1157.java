cfb7c42b437d78bf8ff6d4f2ace11e88fe436dfc
ckcks1997 <ckcks1997@gmail.com>
2021-12-27 18:12:13 +0900

    21.12.27 학습

---
+++ src/baekjoon/s1_word_1157.java
@@ -0,0 +1,33 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+
+public class s1_word_1157 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputVal = scanner.nextLine();
+
+        int[] alphabet = new int[26];
+
+        inputVal=inputVal.toUpperCase();
+        for (char c : inputVal.toCharArray()) {
+            alphabet[c-'A']++;
+        }
+
+        int max = -1;
+        char ch = '?';
+        for (int i = 0; i < alphabet.length; i++) {
+            if(alphabet[i]>max){
+                max = alphabet[i];
+                ch=(char)('A'+i);
+            } else if(alphabet[i]==max)
+                ch = '?';
+        }
+
+        System.out.println(ch);
+
+
+
+    }
+}
