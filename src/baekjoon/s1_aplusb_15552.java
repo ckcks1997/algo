59149baf0cd67ebdbe7786b28aa44842f50019b3
ckcks1997 <ckcks1997@gmail.com>
2021-12-29 17:18:27 +0900

    21.12.29 학습

---
+++ src/baekjoon/s1_aplusb_15552.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+//BufferedReader와 writer 적용 문제
+public class s1_aplusb_15552 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(br.readLine());
+        for (int i = 0; i < count; i++) {
+            String[] in = (br.readLine()).split(" ");
+            bw.write(Integer.parseInt(in[0])+Integer.parseInt(in[1])+"\n");
+        }
+
+        bw.flush();
+    }
+}
